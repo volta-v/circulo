0a26aeafccb1ab6bc9a7d9a8820d7ceac6df4df9
Jorge <vasquez.volta@gmail.com>
2020-06-24 10:50:52 -0600

    ya cuadro

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	let circle = document.querySelector('.circulo')
+	console.log(circle)
+	 circle.style.border = "3px solid red"
+	console.log(circle.style.border)
+}
+
+
+
