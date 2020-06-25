78a288d125e380086f7266e5873450d7ce18b4a9
Jorge <vasquez.volta@gmail.com>
2020-06-25 14:00:51 -0600

    cuadro html

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 window.onload = function () {
 	let circle = document.querySelector('.circulo')
-	console.log(circle)
-	 circle.style.border = "3px solid red"
-	console.log(circle.style.border)
+	circle.style.top = "45%"
+	circle.style.left = "45%"
+	const modifier = 1
+
+	document.addEventListener("keydown", mover) 
+
+		function mover(event) {
+
+			console.log(event)
+			
+
+		if (event.key == "ArrowUp" && parseInt(circle.style.top) > 1) {
+			// console.log("arriba")
+	 	circle.style.top = `${parseInt(circle.style.top) - modifier}%`
+	  } 
+
+	  if (event.key == "ArrowDown") {
+	  	// console.log("abajo")
+	 	circle.style.top = `${parseInt(circle.style.top) + modifier}%`
+	  }
+
+	  if (event.key == "ArrowLeft") {
+	  	// console.log("abajo")
+	 	circle.style.left = `${parseInt(circle.style.left) - modifier}%`
+	  }
+
+	  if (event.key == "ArrowRight") {
+	  	// console.log("abajo")
+	 	circle.style.left = `${parseInt(circle.style.left) + modifier}%`
+	  }
+	}
+
 }
-
-
 
